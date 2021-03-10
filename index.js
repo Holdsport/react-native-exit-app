560266aadcd1a6d8f6a0f71c786685b628187485
Mathias Tambjerg Steengaard <mathiastambjergiversen@gmail.com>
2021-03-10 10:15:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import {NativeModules} from 'react-native';
 var RNExitApp = {
   exitApp: function() {
         NativeModules.RNExitApp.exitApp();
+  },
+  
+  putInBackground: function() {
+    NativeModules.RNExitApp.goToBackGround();
   }
+
 };
 
 export default RNExitApp;
